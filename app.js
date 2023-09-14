ea2cf63ac6a5e230031ac46dd89bb8b275e0efdc
EC2 Default User <noda1028kinni@gmail.com>
2023-09-14 11:26:16 +0000

    add_calculation

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 let hello = "Hello World";
 alert(hello);
+alert("test");
+
+let orange = 100;
+let apple = 10;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if (orange == apple) {
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
