0a932cc1408b11f9c78bf174a5c6d85b388a6f81
EC2 Default User <noda1028kinni@gmail.com>
2023-09-22 11:52:24 +0000

    add_while

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let hello = "Hello World";
 alert(hello);
 alert("test");
 
+
+// 　'if'による条件分岐
 let orange = 100;
 let apple = 10;
 
@@ -12,3 +14,16 @@ if(orange < apple){
 }else{
   alert('みかんの値段がりんごより高い');
 }
+
+
+//　’while’による繰り返し処理
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
