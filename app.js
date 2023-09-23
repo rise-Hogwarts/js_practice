c50f1fcc462cacc461e3580068fe362dde636fa4
EC2 Default User <noda1028kinni@gmail.com>
2023-09-23 14:43:42 +0000

    add_for

--- app.js
+++ app.js
@@ -17,13 +17,22 @@ if(orange < apple){
 
 
 //　’while’による繰り返し処理
-let max = 100;
-let num = 1;
-let count = 0;
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+// while(num < max){
+//   num = num * 3;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('1から10まで足し算した結果は' + num + 'です');
