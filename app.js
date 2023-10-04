0a83363ca0a625f49c8195f83c9bf09dff34370b
EC2 Default User <noda1028kinni@gmail.com>
2023-10-04 11:37:03 +0000

    add_calculation

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-let hello = "Hello World";
-alert(hello);
-alert("test");
+// let hello = "Hello World";
+// alert(hello);
+// alert("test");
 
 
-// 　'if'による条件分岐
-let orange = 100;
-let apple = 10;
+// // 　'if'による条件分岐
+// let orange = 100;
+// let apple = 10;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if (orange == apple) {
-  alert('みかんとりんごが同じ値段');
-}else{
-  alert('みかんの値段がりんごより高い');
-}
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if (orange == apple) {
+//   alert('みかんとりんごが同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
 
 
 //　’while’による繰り返し処理
@@ -28,11 +28,45 @@ if(orange < apple){
 
 // alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-let i;
-let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
-}
 
-alert('1から10まで足し算した結果は' + num + 'です');
+
+//　'for'による決められた回数の繰り返し処理
+
+// let i;        //変数 'i' を宣言
+// let num = 0;  //変数 'num' を宣言 numの初期値を０に設定
+
+// for(i = 1; i < 11; i++){     //for(初期値; 条件式; 増減値)   変数iの初期値を１に設定/変数iの値が11未満のとき/変数iに１を足していく
+//   num = num + i;     //変数numにiを足していった合計が入る.  num = i;だと最終的なiの格納される10という数字だけがnumとして表示される
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+
+
+//javascript2章の確認問題
+
+// let thr = 3;
+// let fiv = 5;
+
+// let plu;
+// let min;
+// let min2;
+// let kake;
+// let wari;
+// let wari2;
+
+// plu = (thr + fiv)
+// min = (thr - fiv)
+// min2 = (fiv - thr)
+// kake = (thr * fiv)
+// wari = (thr / fiv)
+// wari2 = (fiv / thr)
+
+// alert(thr + 'と' + fiv + 'を足した数は' + plu + 'です');
+// alert(thr + 'から' + fiv + 'を引いた数は' + min + 'です');
+// alert(fiv + 'から' + thr + 'を引いた数は' + min2 + 'です');
+// alert(thr + 'と' + fiv + 'をかけた数は' + kake + 'です');
+// alert(thr + 'を' + fiv + 'で割った数は' + wari + 'です');
+// alert(fiv + 'を' + thr + 'で割った数は' + wari2 + 'です');
