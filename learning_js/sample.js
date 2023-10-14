01e47e2e216879a583219242c5d95259c361f0de
EC2 Default User <noda1028kinni@gmail.com>
2023-10-14 06:56:02 +0000

    learning_var-let-const

--- learning_js/sample.js
+++ learning_js/sample.js
@@ -1,2 +1,13 @@
-var nickname ="野田"
-console.log(nickname)
+// 変数において
+// varは再宣言および再代入ができる
+// letは再代入だけできる
+// constは再宣言および再代入のどちらもできない
+const nickname = "taro";
+console.log(nickname);
+
+nickname = "rise";
+console.log(nickname);
+
+// var nickname = "ichiro";
+// console.log(nickname);
+alert(nickname);
