0767370f83eb9f803706dd9f88a6d26ca31b3d92
EC2 Default User <noda1028kinni@gmail.com>
2023-10-16 07:15:35 +0000

    finish[js-2]

--- learning_js/sample.js
+++ learning_js/sample.js
@@ -1,13 +1,31 @@
-// 変数において
-// varは再宣言および再代入ができる
-// letは再代入だけできる
-// constは再宣言および再代入のどちらもできない
-const nickname = "taro";
-console.log(nickname);
+// 　　変数において
+// 　　varは再宣言および再代入ができる
+// 　　letは再代入だけできる
+// 　　constは再宣言および再代入のどちらもできない
+// let nickname = "taro";
+// console.log(nickname);
 
-nickname = "rise";
-console.log(nickname);
+// nickname = "rise";
+// console.log(nickname);
 
 // var nickname = "ichiro";
 // console.log(nickname);
-alert(nickname);
+
+// alert(nickname);
+
+
+// var str = "webcamp" //グローバルスコープ
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+for(let i = 0; i < 10; i++){  // iをletで宣言する理由はループするたびにiに再代入を行うため
+  if(i % 2 == 0){
+    console.log(i);
+  }
+}
+
+//   constは再定義再代入をしていないので値の変更が行われているか
+//   チェックするときに全てのコードを確認する必要はなくなる
